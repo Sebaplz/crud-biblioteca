b6aaf71a33cca4ee0b2883939f74e9ad96805c85
Sebaplz <sebastian.h.neira@outlook.com>
2024-01-09 18:41:49 -0300

    Cambios en la ui book

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -3,15 +3,18 @@ import { Link } from "react-router-dom";
 /* eslint-disable react/prop-types */
 export default function Book({ book }) {
   return (
-    <Link to={`/book/${book.id}`}>
-      <li className="flex flex-col items-center">
-        <img
-          src={book.imagen}
-          alt={`Imagen del libro ${book.nombre}`}
-          className="h-60 w-52 rounded-lg object-center"
-        />
-        <h2 className="text-center">{book.nombre}</h2>
+    <>
+      <li className="flex h-[300px] flex-col items-center justify-center rounded-lg">
+        <Link to={`/book/${book.id}`}>
+          <img
+            src={book.imagen}
+            alt={`Imagen del libro ${book.nombre}`}
+            className="h-60 w-52 rounded-lg object-center"
+          />
+        </Link>
+        <h2 className="w-52 truncate text-left font-semibold">{book.nombre}</h2>
+        <h3 className="w-52 truncate text-left">{book.autor}</h3>
       </li>
-    </Link>
+    </>
   );
 }
