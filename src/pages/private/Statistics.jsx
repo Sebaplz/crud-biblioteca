df70022f46ab76d67cc44257f270f187715832ed
Sebaplz <sebastian.h.neira@outlook.com>
2024-01-18 10:55:40 -0300

    Se agrega grafico BarList a Statistics

--- src/pages/private/Statistics.jsx
+++ src/pages/private/Statistics.jsx
@@ -1,6 +1,14 @@
 import { useEffect, useState } from "react";
 import Loading from "../../util/Loading";
-import { Card, DonutChart, Title } from "@tremor/react";
+import {
+  BarList,
+  Bold,
+  Card,
+  DonutChart,
+  Flex,
+  Text,
+  Title,
+} from "@tremor/react";
 import { Link } from "react-router-dom";
 import { IconArrowBackUp } from "@tabler/icons-react";
 
@@ -28,6 +36,13 @@ export default function Statistics() {
     fetchBooks();
   }, []);
 
+  const transformedJson = books.map((book) => ({
+    key: book.id,
+    value: book.downloads,
+    name: book.title,
+    icon: null,
+  }));
+
   return (
     <>
       {error && <p className="text-red-500">{error}</p>}
@@ -36,12 +51,24 @@ export default function Statistics() {
       ) : (
         <>
           <main className="mx-auto max-w-5xl pt-32">
-            <div className="flex">
+            <div className="flex pb-4">
               <Link to={"/dashboard"}>
                 <IconArrowBackUp color="#e02957" size={30} />
               </Link>
             </div>
-            <section className="flex flex-col items-center p-4">
+            <section className="flex items-start gap-4">
+              <Card className="max-w-lg">
+                <Title>Todos los libros</Title>
+                <Flex className="mt-4">
+                  <Text>
+                    <Bold>Libros</Bold>
+                  </Text>
+                  <Text>
+                    <Bold>Descargas</Bold>
+                  </Text>
+                </Flex>
+                <BarList data={transformedJson} className="mt-2" />
+              </Card>
               <Card className="max-w-md">
                 <Title className="text-center">Libros descargados</Title>
                 <DonutChart
